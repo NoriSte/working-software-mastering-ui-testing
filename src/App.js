7d0ae5beffc6ccbd9cf1916e0ef58fb5518474ea
NoriSte <nori.ste.magni@gmail.com>
2019-06-21 15:28:50 +0200

    Store the input field values

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import logo from "./logo.svg";
 import { LOGIN_BUTTON, PASSWORD_PLACEHOLDER, USERNAME_PLACEHOLDER } from "./strings";
 
 function App() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +14,17 @@ function App() {
       </header>
       <section className="App-body">
         <span>Please type</span>
-        <input placeholder={USERNAME_PLACEHOLDER} />
-        <input placeholder={PASSWORD_PLACEHOLDER} type="password" />
+        <input
+          placeholder={USERNAME_PLACEHOLDER}
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          placeholder={PASSWORD_PLACEHOLDER}
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
         <button>{LOGIN_BUTTON}</button>
       </section>
     </div>
